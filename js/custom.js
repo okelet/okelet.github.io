9dc4466fb77ed276ad015a07153ba3d748a4e4c8
okelet <okelet@users.noreply.github.com>
2020-02-11 13:25:53 +0000

    deploy: okelet/blog@94293ebf82b173d27e800d92f66539dca1939908

--- js/custom.js
+++ js/custom.js
@@ -5,7 +5,7 @@ $( document ).ready(function() {
     $('a[href^="http://"],a[href^="https://"]').each(function() {
         if (!$(this).hasClass("iconfont")) {
             $(this).css({
-                background: "url(http://www.google.com/s2/favicons?domain=" + this.hostname + ") left center no-repeat",
+                background: "url(https://www.google.com/s2/favicons?domain=" + this.hostname + ") left center no-repeat",
                 "padding-left": "20px"
             });
         }
